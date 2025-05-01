791d90a92ad82909a8da88028972b67a92f2c7ec
Tom Planche <Tomplanche@icloud.com>
2025-05-01 14:36:50 +0200

    [24] (feat on main) Font does not crash if no back runs.

--- front/src/lib/stores/tasks.ts
+++ front/src/lib/stores/tasks.ts
@@ -1,5 +1,5 @@
-import { writable } from 'svelte/store';
-import type {TTask, TTasksResponse} from "$lib/types/task";
+import { writable } from "svelte/store";
+import type { TTask, TTasksResponse } from "$lib/types/task";
 
 const createTasksStore = () => {
     const { subscribe, set, update } = writable<TTasksResponse>();
@@ -7,38 +7,45 @@ const createTasksStore = () => {
     return {
         subscribe,
         set: (value: TTasksResponse) => {
+            if (!value.tasks) return;
+
             // Sort tasks by descending ID when setting initial value
             const sortedTasks = {
                 ...value,
-                tasks: [...value.tasks].sort((a, b) => b.id - a.id)
+                tasks: [...value.tasks].sort((a, b) => b.id - a.id),
             };
             set(sortedTasks);
         },
-        addTask: (task: TTask) => update(state => {
-            if (!state) return state;
-            
-            return {
-                ...state,
-                tasks: [task, ...state.tasks], // Add new task at the beginning
-            };
-        }),
-        deleteTask: (id: number) => update(state => {
-            if (!state || !state.tasks.length) return state;
+        addTask: (task: TTask) =>
+            update((state) => {
+                if (!state) return state;
 
-            return {
-                ...state,
-                tasks: state.tasks.filter(task => task.id !== id),
-            };
-        }),
-        updateTask: (task: TTask) => update(state => {
-            if (!state) return state;
+                return {
+                    ...state,
+                    tasks: [task, ...state.tasks], // Add new task at the beginning
+                };
+            }),
+        deleteTask: (id: number) =>
+            update((state) => {
+                if (!state || !state.tasks.length) return state;
 
-            return {
-                ...state,
-                tasks: state.tasks.map(t => t.id === task.id ? task : t)
-            };
-        }),
+                return {
+                    ...state,
+                    tasks: state.tasks.filter((task) => task.id !== id),
+                };
+            }),
+        updateTask: (task: TTask) =>
+            update((state) => {
+                if (!state) return state;
+
+                return {
+                    ...state,
+                    tasks: state.tasks.map((t) =>
+                        t.id === task.id ? task : t,
+                    ),
+                };
+            }),
     };
-}
+};
 
-export const tasksStore = createTasksStore(); 
+export const tasksStore = createTasksStore();
